8dade35d958c35ceb4f1eb3b30752b578ceb9bbf
Muhammad Xamidov <muhammadxamidov001@gmail.com>
2023-10-10 09:19:27 +0300

    Dark mode working with local storage

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
-const elSiteThemeToggler = document.querySelector('.site-header-theme-toggler');
+const elThemeToggler = document.querySelector('.js-theme-toggler');
 
-elSiteThemeToggler.addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode')
-})
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    elThemeToggler.textContent = "Light Mode";
+}
+
+if (elThemeToggler) {
+    elThemeToggler.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+        if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("theme", "dark");
+            elThemeToggler.textContent = "Light Mode";
+        } else {
+            localStorage.setItem("theme", "light");
+            elThemeToggler.textContent = "Dark Mode";
+        }
+    });
+}
